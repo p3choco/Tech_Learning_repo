f93c438fe50e1b1ded3dc49ce4f96339b3a64b5e
p3choco <62072811+p3choco@users.noreply.github.com>
2025-04-08 18:09:14 +0200

    categories work

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -7,18 +7,24 @@ import dev.kord.gateway.Intent
 import dev.kord.gateway.PrivilegedIntent
 import kotlinx.coroutines.runBlocking
 
+private val categories = listOf("Elektronika", "RTV/AGD", "Książki")
 
 object Bot {
     fun startBot(botToken: String) = runBlocking {
         val kord = Kord(botToken)
 
         kord.on<MessageCreateEvent> {
-            if (message.author?.isBot != false) return@on
+            if (message.author?.isBot == true) return@on
 
             val content = message.content
-
-            if (content.startsWith("!bot")) {
-                message.channel.createMessage("Hej, otrzymałem wiadomość: $content")
+            when {
+                content.startsWith("!bot") -> {
+                    message.channel.createMessage("Hej, otrzymałem wiadomość: $content")
+                }
+                content.startsWith("!kategorie") -> {
+                    val response = "Dostępne kategorie: ${categories.joinToString(", ")}"
+                    message.channel.createMessage(response)
+                }
             }
         }
 
