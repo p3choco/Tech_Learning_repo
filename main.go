34d847137ee7f94a6d72375a74a168805c4ebffd
p3choco <62072811+p3choco@users.noreply.github.com>
2025-04-10 12:22:53 +0200

    third commit go

--- main.go
+++ main.go
@@ -17,6 +17,12 @@ type Product struct {
 	Price float64 `json:"price"`
 }
 
+type Cart struct {
+	gorm.Model
+	UserID    uint    `json:"user_id"`
+	CartValue float64 `json:"cart_value"`
+}
+
 func main() {
 	initDB()
 
@@ -28,6 +34,9 @@ func main() {
 	e.PUT("/products/:id", updateProduct)
 	e.DELETE("/products/:id", deleteProduct)
 
+	e.GET("/carts", getCarts)
+	e.POST("/carts", createCart)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
@@ -38,7 +47,7 @@ func initDB() {
 		panic("failed to connect database")
 	}
 
-	DB.AutoMigrate(&Product{})
+	DB.AutoMigrate(&Product{}, &Cart{})
 }
 
 func getProducts(c echo.Context) error {
@@ -101,4 +110,23 @@ func deleteProduct(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
 	}
 	return c.NoContent(http.StatusNoContent)
+}
+
+func getCarts(c echo.Context) error {
+	var carts []Cart
+	if err := DB.Find(&carts).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
+	}
+	return c.JSON(http.StatusOK, carts)
+}
+
+func createCart(c echo.Context) error {
+	var cart Cart
+	if err := c.Bind(&cart); err != nil {
+		return err
+	}
+	if err := DB.Create(&cart).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
+	}
+	return c.JSON(http.StatusCreated, cart)
 }
