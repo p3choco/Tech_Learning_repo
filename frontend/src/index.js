5d59c24ba259d44bbd893c62a3f14d343b428161
p3choco <62072811+p3choco@users.noreply.github.com>
2025-04-24 01:24:40 +0200

    commit 5

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+
+import './axiosConfig';
+
 import App from './App';
 import './index.css';
-
 import { CartProvider } from './context/CartContext';
 
 const container = document.getElementById('root');
